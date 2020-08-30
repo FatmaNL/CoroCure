53ce4bc22b23f4977eea82c996ad3740fdf3fb2b
Fatma Naili <fatma4497@gmail.com>
2020-08-30 19:43:25 +0100

    exporter

--- angular/coro-cure/src/app/app.module.ts
+++ angular/coro-cure/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -34,6 +34,7 @@ import { ExportationComponent } from './exportation/exportation.component';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     NgxSpinnerModule,
     CommonModule,
     BrowserAnimationsModule, // required animations module
--- angular/coro-cure/src/app/coronarographie/coronarographie.component.ts
+++ angular/coro-cure/src/app/coronarographie/coronarographie.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { TabCardiologue } from 'src/app/cardiologue/cardiologue';
 import { CardiologueService } from 'src/app/cardiologue/cardiologue.service';
 import { NgxSpinnerService } from 'ngx-spinner';
-import { FormGroup} from '@angular/forms';
+import { FormGroup, FormBuilder, FormArray} from '@angular/forms';
 
 @Component({
   selector: 'app-coronarographie',
@@ -14,14 +14,16 @@ export class CoronarographieComponent implements OnInit {
   cardiologue: TabCardiologue = new TabCardiologue();
   errorMessage: string;
   schemaUrl: string = 'assets/schema.png';
-  
+  lesionForms: FormArray = this.fb.array([]);
 
-  constructor(private cardiologueservice: CardiologueService, private spinner: NgxSpinnerService) { }
+  constructor(private cardiologueservice: CardiologueService, 
+              private spinner: NgxSpinnerService,
+              private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.getCardiologues();
+    this.addLesionForm();
   }
-
 
   getCardiologues(): void {
     this.spinner.show();
@@ -29,6 +31,17 @@ export class CoronarographieComponent implements OnInit {
       next: cardiologues => { this.cardiologues = cardiologues; this.spinner.hide(); },
       error: err => {this.errorMessage = err; this.spinner.hide(); }
     });
+  }
+
+  addLesionForm() {
+    this.lesionForms.push(this.fb.group({
+      lesionID: [0],
+      type: [''],
+      degre: [''],
+      fluxTIMI: [''],
+      descriptionId: [0],
+      descriptionSegment: ['']
+    }));
   }
 
 }
